d4602262cc10692f1b73887c94416cd44e1a04ba
Alina Alieks <alieksieienko.alina@gmail.com>
2024-05-07 00:44:58 +0300

    initial project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { Layout } from "../Layout/Layout";
+import { AppBar } from "../AppBar/AppBar";
+import { TaskForm } from "../TaskForm/TaskForm";
+import { TaskList } from "../TaskList/TaskList";
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
+  );
+}
